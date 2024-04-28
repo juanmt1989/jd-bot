122be52c72cd648eb4f00d62a0078f6b6b378dd1
johmikepty <juanmt1989@gmail.com>
2024-04-28 09:22:00 -0500

    Restoring material ui

--- src/popup/App/AddUser.tsx
+++ src/popup/App/AddUser.tsx
@@ -1,27 +1,26 @@
-// import React, { useState } from 'react';
-// import IconButton from '@mui/material/IconButton';
-// import AddIcon from '@mui/icons-material/Add';
-// import BlockIcon from '@mui/icons-material/Block';
-// import {Sender} from './../distpacher/popupservice'
-// import {CustomerAction}  from '../../helpers/commonobjects/customeraction'
+import React, { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
+import BlockIcon from '@mui/icons-material/Block';
+import {Sender} from './../distpacher/popupservice'
+import {CustomerAction}  from '../../helpers/commonobjects/customeraction'
 
-// const AddUser = () => {
-//   const [clicked, setClicked] = useState(false);
-//    console.log('def add user button..')
-//   const handleIconClick = () => {
-//     setClicked(!clicked); 
-//     console.log('add user button..')
-//     Sender(CustomerAction.GetCustomer)
+const AddUser = () => {
+  const [clicked, setClicked] = useState(false);
+   console.log('def add user button..')
+  const handleIconClick = () => {
+    setClicked(!clicked); 
+    console.log('add user button..')
+    Sender(CustomerAction.GetCustomer)
     
-//   };
+  };
 
-//   return (
-//     <IconButton onClick={handleIconClick}>
-//       {clicked ? <BlockIcon /> : <AddIcon />}
-//     </IconButton>
-//   );
-// };
+  return (
+    <IconButton onClick={handleIconClick}>
+      {clicked ? <BlockIcon /> : <AddIcon />}
+    </IconButton>
+  );
+};
 
-// export default AddUser;
+export default AddUser;
 
-export {}
--- src/popup/App/App.tsx
+++ src/popup/App/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
 import { Counter } from '../Counter'
-//import FabIntegrationSnackbar from './menu'
+import FabIntegrationSnackbar from './menu'
+
 function App() {
   return (
     <div className="App">
-      {/* <FabIntegrationSnackbar /> */}
+      <FabIntegrationSnackbar />
     </div>
   )
 }
